4a56e577d45f6160f132bf9339d1f1afa3c1150d
LeagueOfFreeWorlds <knowledgelord12@gmail.com>
2021-12-19 19:35:27 -0700

    Started my first CPP project... pretty lame I know.

--- Heythere/src/Heythere.cpp
+++ Heythere/src/Heythere.cpp
@@ -8,8 +8,24 @@
 
 #include <iostream>
 using namespace std;
-
+int myNumber;
+int first;
+int second;
+string result;
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	myNumber = 10;
+	first = 15;
+	second = 15;
+	string myString = "abcdefg";
+	cout << "Hello there\tI am your computer talking." << endl; // prints stuff to the console.
+	cout << 24 * 5 / 3 - 21 * 0.5 << endl;
+	cout << myNumber << endl;
+	myNumber *= 2;
+	cout << myNumber << endl;
+	myString[1] = 'de';
+	cout << myString[2] << endl;
+	cout << myString << endl;
+	result = (first == second) ? "equal" : "not equal";
+	cout << result << endl;
 	return 0;
 }
